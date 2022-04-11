b5d1fba4be127ffaef12b0cc54b5bcdad478cb7f
Allu-Arjun-Dev <102848810+Allu-Arjun-Dev@users.noreply.github.com>
2022-04-11 17:17:30 +0530

    Create translation.py

---
+++ translation.py
@@ -0,0 +1,60 @@
+class LuciferMoringstar(object):
+
+    DEFAULT_MSG = """👋Hello {mention}.....!!!\nIt's Power Full [{bot_name}](t.me/{bot_username}) Here 😎\nAdd Me To Your Group And Make Sure I'm an Admin There! \nAnd Enjoy My Pever Show.....!!!🤪"""
+
+    HELP_MSG = """**Help Veno Monuse 🤣**"""
+
+    ABOUT_MSG = """
+🤖 My Name [{bot_name}](t.me/{bot_username})
+
+👑 My Creator : [{dev_name}]
+
+🛠️ My Library : [Pyrogram]
+
+🈵 My Language : [Python3]
+
+🗃️ My Database : [Mongodb]
+
+📡 My Server : [Heroku]"""
+
+    FILE_CAPTIONS = """📁 File Info - <code>{file_name}</code>
+========================
+🔘 Join
+👉<a href=https://t.me/Its_unique_movies_adda >PANCH MISALI</a>
+👉<a href=https://t.me/+BixProBUQBo4MWM9 >UNIQUE MOVIE UPDATES</a>
+========================
+<a href=http://t.me/Panchmisali_7_Bot >Request Bot</a>
+<a href=https://t.me/Panchmisali_2_Bot>Channel Bot</a>
+========================= 
+ JOIN 🎗 SHARE 🎗 SUPPORT
+========================="""
+
+    PR0FESS0R_99 = """
+**ADMINS COMMANDS**
+
+ » /broadcast - Reply Any Media Or Message
+ » /delete - Reply Files
+ » /deleteall - Delete All Files
+ » /total - How Many Files Saved
+ » /logger - Get Bot Logs
+ » /channel - Add Channel List"""
+
+    ADD_YOUR_GROUP = """**__ഞാൻ ജോലി ചെയ്യുന്ന ഗ്രൂപ്പിൽ ചേരൂ, നിങ്ങൾക്ക് ആവശ്യമുള്ള എല്ലാ സിനിമകളും ആ ഗ്രൂപ്പിൽ അഭ്യർത്ഥിക്കാം! ഇവിടെ അഭ്യർത്തിക്കരുത്..😤__**"""
+    SPELL_CHECK = """
+**Hello 👋{mention}**,
+
+**Couldn't Find {query}?  Please Click Your Request Movie Name**"""
+    GET_MOVIE_1 = """
+📽️ **Requested** : [{query}]({url})
+👤 **Requested By** : {mention}
+🎬 **Title** : [{title}]({url})
+📆 **Year** : {year}
+🌟 **Rating** {rating}/10
+🎭 **Genres** {genres}"""
+
+
+    GET_MOVIE_2 = """
+📽️ **Requested** : {query}
+👤 **Requested By** : {mention}
+
+© **{chat}**"""
